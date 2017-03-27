de07da1f2a94d8fb3f21ad39188d1495247333f1
Vladimir Bajceta <bajceta@gmail.com>
2017-03-27 16:29:23 +0200

    malo preslozeno

--- todo.js
+++ todo.js
@@ -1,34 +1,39 @@
- // Triger event on enter
-$(document).keypress(function(e) {
-    if(e.which == 13) {
-       var Article =  $('.newArticle input').val();
-       var addArticleToTheList = $('<li> <span class="item"> '+Article+' </span> <span class="remove"> X </span> </li>');
-       $('.alreadyAddedArticles').find('ul').append(addArticleToTheList);
-       $('.newArticle input').val('');
-       $(addArticleToTheList).on('click', '.remove' , function(){
-            $(this).parent().hide();
-        });
-       $(addArticleToTheList).on('click', '.item' , function(){
-            $(this).replaceWith('<input value="'+Article+'">');
-            
-        });
+function addNewItem(){
+    var newItemText =  $('.newArticle input').val();
+    var addArticleToTheList = $('<li> <span class="item"> '+newItemText+' </span> <span class="remove"> X </span> </li>');
+    $('.alreadyAddedArticles').find('ul').append(addArticleToTheList);
+    $('.newArticle input').val('');
+    $(addArticleToTheList).on('click', '.remove' , function(){
+        $(this).parent().hide();
+    });
+}
+
+function onEnter(fn){
+    return function(e) {
+        if(e.which== 13) {
+            fn();
+        }
     }
-});
+}
+
+function editItem(){
+    console.log(this);
+    var text = $(this).text();
+    console.log('Edit item : ' + text);
+    var inputField = $('<input type="text" value="'+text+'" />');
+    $(this).replaceWith(inputField);
+}
+
+function stopEditing(){
+    console.log(this)
+    var newValue = $(this).val();
+    var itemSpan = $(' <span class="item"> '+ newValue+' </span> ');
+    $(this).replaceWith(itemSpan);
+}
 
 $(document).ready(function(){
-    $('.newArticle').on('click', 'button', function(){
-       var Article =  $('.newArticle input').val();
-       var addArticleToTheList = $('<li> <span class="item"> '+Article+' </span> <span class="remove"> X </span> </li>');
-       $('.alreadyAddedArticles').find('ul').append(addArticleToTheList);
-       $('.newArticle input').val('');
-       $(addArticleToTheList).on('click', '.remove' , function(){
-            $(this).parent().hide();
-        });
-       $('.alreadyAddedArticles').on('click', '.item' , function(){ 
-        var additedArticle = $('<li> <span class="item"> '+newValue+' </span> <span class="remove"> X </span> </li>');
-            $(this).replaceWith('<input value='+Article+'>');
-            var newValue = $(this).val();
-            $(this).parent().replaceWith(additedArticle);
-        });
-    });    
+    $('.newArticle').on('click', 'button', addNewItem);
+    $('.newArticle').on('keypress', 'input', onEnter(addNewItem));
+    $('.alreadyAddedArticles').on('click', '.item' , editItem);
+    $('.alreadyAddedArticles').on('focusout', 'input' , stopEditing);
 });
