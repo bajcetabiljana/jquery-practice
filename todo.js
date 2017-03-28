811c3d8b5340960ae7d71404960482891f42e391
Biljana Bajceta <biljanara@yahoo.com>
2017-03-28 07:25:29 +0200

    sa tvog kompa

--- todo.js
+++ todo.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
     $('.newArticle').on('click', 'button', function(){
-       var Article =  $('input').val();
-       var addArticleToTheList = $('<li>'+Article+'</li>');
+        console.log('pokaze ovaj tekst');
+       console.log(this);
+       console.log(Article);
+
+       var Article =  $(this).parent().find('input').val();
+       console.log(Article);
+       var addArticleToTheList = $('<li><span class="item">'+Article+'</span> <span class="removeButton"> X </span> </li>');
        $('.alreadyAddedArticles').find('ul').append(addArticleToTheList);
-       $('input').val('');
+       $(this).parent().find('input').val('');
+    });
+
+     $('.alreadyAddedArticles').on('click', '.removeButton', function(){
+        console.log('test');
+        $(this).parents('li').remove();
+    });
+     $('.alreadyAddedArticles').on('click', '.item', function(){
+        var oldText = $(this).text();
+        var inputField = $('<input>');
+        inputField.val(oldText);
+        inputField.on('focusout', function(){
+            var text = $(this).val();
+            $(this).replaceWith('span class="item">'+text+'</span> ');
+        });
+        //$(this).replaceWith('<input value="'+oldText+'">');
+        $(this).replaceWith(inputField);
     });
 });
